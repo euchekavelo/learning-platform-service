79fff98c3dc5a7b6cd050137b72056a657c75c38
Kirill Suslov <euchekavelo@gmail.com>
2025-11-07 15:35:39 +0300

    Added basic project structure.

---
+++ src/main/java/ru/mephi/learningplatformservice/LearningPlatformServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.mephi.learningplatformservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningPlatformServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningPlatformServiceApplication.class, args);
+	}
+
+}
